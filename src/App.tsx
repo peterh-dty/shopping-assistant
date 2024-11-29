ab6b133ac9905b5ce62c8dbbb9360d723e8c1305
peterh-dty <peter.h@deity.io>
2024-11-29 12:41:22 +0000

    - fixed the navbar - added mapbox api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./components/home";
-import routes from "tempo-routes";
+
+const basename = import.meta.env.BASE_URL;
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+      <BrowserRouter basename={basename}>
+        <Home />
+      </BrowserRouter>
     </Suspense>
   );
 }
